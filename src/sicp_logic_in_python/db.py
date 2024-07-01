9d64507c2c69f51188de1cae6b61d56011315d5e
volesen <vincolesen@gmail.com>
2024-07-01 15:41:45 +0200

    Simplify DB interface (skip indexing)

--- src/sicp_logic_in_python/db.py
+++ src/sicp_logic_in_python/db.py
@@ -8,22 +8,10 @@ class DB:
     def fetch_assertions(self, pattern, frame) -> Iterable[tuple]:
         raise NotImplementedError
 
-    def get_all_assertions(self) -> Iterable[tuple]:
-        raise NotImplementedError
-
-    def get_indexed_assertions(self, index) -> Iterable[tuple]:
-        raise NotImplementedError
-
     def add_rule(self, rule: tuple):
         raise NotImplementedError
 
     def fetch_rules(self, pattern, frame) -> Iterable[tuple]:
-        raise NotImplementedError
-
-    def get_all_rules(self) -> Iterable[tuple]:
-        raise NotImplementedError
-
-    def get_indexed_rules(self, index) -> Iterable[tuple]:
         raise NotImplementedError
 
 
@@ -36,22 +24,10 @@ class MemoryDB(DB):
         self.assertions.append(assertion)
 
     def fetch_assertions(self, pattern, frame) -> Iterable[tuple]:
-        return self.get_all_assertions()
-
-    def get_all_assertions(self) -> Iterable[tuple]:
         return self.assertions
-
-    def get_indexed_assertions(self, index) -> Iterable[tuple]:
-        return self.get_all_assertions()
 
     def add_rule(self, rule: tuple):
         self.rules.append(rule)
 
     def fetch_rules(self, pattern, frame) -> Iterable[tuple]:
-        return self.get_all_rules()
-
-    def get_all_rules(self) -> Iterable[tuple]:
         return self.rules
-
-    def get_indexed_rules(self, index) -> Iterable[tuple]:
-        return self.get_all_rules()
